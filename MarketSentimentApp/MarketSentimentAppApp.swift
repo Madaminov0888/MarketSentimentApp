d3aea908d00bd247f6e714acfb3a233abb91a3ad
Madaminov Muhammadjon <90606773+Madaminov0888@users.noreply.github.com>
2025-12-02 13:14:45 +0500

    Initial Commit

---
+++ MarketSentimentApp/MarketSentimentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketSentimentAppApp.swift
+//  MarketSentimentApp
+//
+//  Created by Muhammadjon Madaminov on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MarketSentimentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
